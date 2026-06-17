82826e3416afe424d1462ff08caa3bd5af523ff0
ALahmady1 <mo1sa8@icloud.com>
2026-06-18 00:04:10 +0300

    fix admin context

--- flask_backend/app.py
+++ flask_backend/app.py
@@ -29,14 +29,14 @@ def create_app():
        # seed_database()
         from flask_backend.models import Admin
 
-    admin = Admin.query.filter_by(username="Ak47").first()
-    if not admin:
-        admin = Admin(
-            username="Ak47",
-            password_hash=bcrypt.generate_password_hash("black&white2027").decode("utf-8")
-        )
-        db.session.add(admin)
-        db.session.commit()
+        admin = Admin.query.filter_by(username="Ak47").first()
+        if not admin:
+            admin = Admin(
+                username="Ak47",
+                password_hash=bcrypt.generate_password_hash("black&white2027").decode("utf-8")
+            )
+            db.session.add(admin)
+            db.session.commit()
         return app
 app = create_app()
 
