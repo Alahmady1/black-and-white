0c8a9b591cec50840b757de70f38921d34ca5442
ALahmady1 <mo1sa8@icloud.com>
2026-06-17 23:08:15 +0300

    Use PostgreSQL database

--- flask_backend/config.py
+++ flask_backend/config.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-bw-key-2026'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///bw_restaurant.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PERMANENT_SESSION_LIFETIME = timedelta(days=7)
     UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static', 'uploads')
