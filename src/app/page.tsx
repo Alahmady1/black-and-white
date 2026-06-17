8d26df7632710c121f8085bc627d75c518ff2cfe
ALahmady1 <mo1sa8@icloud.com>
2026-06-17 20:24:26 +0300

    Redirect home to auth page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,5 @@
-import Navbar from "@/components/Navbar";
-import Hero from "@/components/Hero";
-import Menu from "@/components/Menu";
-import Contact from "@/components/Contact";
-import Footer from "@/components/Footer";
-import FloatingActions from "@/components/WhatsAppButton";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main className="bg-white min-h-screen">
-      <Navbar />
-      <Hero />
-      <Menu />
-      <Contact />
-      <Footer />
-      <FloatingActions />
-    </main>
-  );
+  redirect("/auth.html");
 }
